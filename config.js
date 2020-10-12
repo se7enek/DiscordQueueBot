a8511b6efc36488895d3397f92aeb64f55788171
se7en <k.martulewicz@gmail.com>
2020-10-12 12:42:29 +0200

    Merge conflicts

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-	prefix: '!',
-	token: 'secret',
-	reactions: ['aukill1', 'klepsydra', 'aukill2'],
+	prefix: "!",
+	token: "NzY0NDUyNzEyMjIxNzY5NzM4.X4GeEQ.Vi5AIe3C6lpfo5k8AOA3ppR81Hg",
+	emojiname: ["aukill1", "klepsydra", "aukill2"],
 });
